f35f23810fe8dbb9096bafb0d4f94c013c1272d8
Michael Doyle <mikaldoyle@gmail.com>
2021-02-26 15:49:57 -0800

    database work in progress

---
+++ backend/api/models/ProjectTeam.js
@@ -0,0 +1,87 @@
+/**
+ * ProjectTeam.js
+ *
+ * Information for each project team in Capstone.
+ */
+
+module.exports = {
+
+    attributes: {
+  
+      //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+      //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+      //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+      projectTitle: {
+        type: 'Character',
+        required: true,
+        description: "Title of project",
+        manLength: 200, 
+        example: "Reimagining the iSchool Capstone Archive"
+      },  
+
+      sponsorOrganization: {
+        type: 'character',
+        required: true,
+        description: "Name of the sponsored organization the group is working with. N/A for non-sponsored",
+        maxLength: 120, 
+        example: 'University of Washington'
+      },
+      
+      tileImage: {
+        type: 'file',
+        required: true,
+        description: "Image for project tile in archive browsing page",
+      },
+
+      posterImage: {
+        type: 'file',
+        required: true,
+        description: "Image for project poster on project display page",
+      },
+
+      projectPoster: {
+        type: 'file',
+        required: true,
+        description: "Uploaded file of project poster",
+      },
+
+      projectPoster: {
+        type: 'file',
+        required: true,
+        description: "Uploaded file of project poster",
+      },
+
+      websiteURL: {
+        type: 'character',
+        required: false,
+        description: "URL of project website if group has made one",
+        example: 'exampleproject.com'
+      },
+
+      iSchoolPermission: {
+        type: 'boolean',
+        required: true,
+        description: "Gives permission for project to be included on iSchool website",
+      },
+
+      eventParticipation: {
+        type: 'boolean',
+        required: true,
+        description: "Yes/No for participating in the Capstone Event",
+      },
+      //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+      //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+      //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+      // n/a
+  
+      //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+      //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+      //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+      // n/a
+  
+    },
+  
+  
+  };
+  
