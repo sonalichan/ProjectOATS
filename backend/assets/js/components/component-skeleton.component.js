e5752d3188839b66883016241430058d874f2cf8
erinRochfort99 <erochfor@uw.edu>
2021-03-02 15:00:19 -0800

    added component skeleton files

---
+++ backend/assets/js/components/component-skeleton.component.js
@@ -0,0 +1,60 @@
+/**
+ * TODO: change the component name to match--use kebab-case here
+ * This is how you will reference your component
+ * 
+ * <your-component-name>
+ * -----------------------------------------------------------------------------
+ * A button with a built-in loading spinner.
+ *
+ * @type {Component}
+ *
+ * @event click   [emitted when clicked]
+ * -----------------------------------------------------------------------------
+ */
+
+// TODO: change the component name to match file name--use camelCase here
+parasails.registerComponent('yourComponentName', {
+    //  ╔═╗╦═╗╔═╗╔═╗╔═╗
+    //  ╠═╝╠╦╝║ ║╠═╝╚═╗
+    //  ╩  ╩╚═╚═╝╩  ╚═╝
+    props: [
+
+    ],
+  
+    //  ╦╔╗╔╦╔╦╗╦╔═╗╦    ╔═╗╔╦╗╔═╗╔╦╗╔═╗
+    //  ║║║║║ ║ ║╠═╣║    ╚═╗ ║ ╠═╣ ║ ║╣
+    //  ╩╝╚╝╩ ╩ ╩╩ ╩╩═╝  ╚═╝ ╩ ╩ ╩ ╩ ╚═╝
+    data: function (){
+      return {
+        //…
+      };
+    },
+  
+    //  ╦ ╦╔╦╗╔╦╗╦
+    //  ╠═╣ ║ ║║║║
+    //  ╩ ╩ ╩ ╩ ╩╩═╝
+    template: `
+    <div>
+    </div>
+    `,
+  
+    //  ╦  ╦╔═╗╔═╗╔═╗╦ ╦╔═╗╦  ╔═╗
+    //  ║  ║╠╣ ║╣ ║  ╚╦╝║  ║  ║╣
+    //  ╩═╝╩╚  ╚═╝╚═╝ ╩ ╚═╝╩═╝╚═╝
+    beforeMount: function() {
+      //…
+    },
+    mounted: async function(){
+      //…
+    },
+    beforeDestroy: function() {
+      //…
+    },
+  
+    //  ╦╔╗╔╔╦╗╔═╗╦═╗╔═╗╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ║║║║ ║ ║╣ ╠╦╝╠═╣║   ║ ║║ ║║║║╚═╗
+    //  ╩╝╚╝ ╩ ╚═╝╩╚═╩ ╩╚═╝ ╩ ╩╚═╝╝╚╝╚═╝
+    methods: {
+    }
+  });
+  
