4735594d5204163d613c73602f689b83f2542af4
erinRochfort99 <erochfor@uw.edu>
2021-03-03 20:45:38 -0800

    add two new pages for our two designs

---
+++ backend/api/controllers/view-old-data-page.js
@@ -0,0 +1,27 @@
+module.exports = {
+
+
+  friendlyName: 'View old data page',
+
+
+  description: 'Display "Old data" page.',
+
+
+  exits: {
+
+    success: {
+      viewTemplatePath: 'pages/old-data-page'
+    }
+
+  },
+
+
+  fn: async function () {
+
+    // Respond with view.
+    return {};
+
+  }
+
+
+};
