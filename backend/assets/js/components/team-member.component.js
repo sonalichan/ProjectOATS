8da09c419d85570e2fd610d51fa623becbcb681f
erinRochfort99 <erochfor@uw.edu>
2021-03-04 16:51:11 -0800

    created new member component

---
+++ backend/assets/js/components/team-member.component.js
@@ -0,0 +1,63 @@
+/**
+ * TODO: change the component name to match--use kebab-case here
+ * This is how you will reference your component
+ * 
+ * <team-member>
+ * -----------------------------------------------------------------------------
+ * A button with a built-in loading spinner.
+ *
+ * @type {Component}
+ *
+ * @event click   [emitted when clicked]
+ * -----------------------------------------------------------------------------
+ */
+
+// TODO: change the component name to match file name--use camelCase here
+parasails.registerComponent('teamMember', {
+    //  ╔═╗╦═╗╔═╗╔═╗╔═╗
+    //  ╠═╝╠╦╝║ ║╠═╝╚═╗
+    //  ╩  ╩╚═╚═╝╩  ╚═╝
+    props: [
+        "name",
+        "role"
+    ],
+  
+    //  ╦╔╗╔╦╔╦╗╦╔═╗╦    ╔═╗╔╦╗╔═╗╔╦╗╔═╗
+    //  ║║║║║ ║ ║╠═╣║    ╚═╗ ║ ╠═╣ ║ ║╣
+    //  ╩╝╚╝╩ ╩ ╩╩ ╩╩═╝  ╚═╝ ╩ ╩ ╩ ╩ ╚═╝
+    data: function (){
+      return {
+        //…
+      };
+    },
+  
+    //  ╦ ╦╔╦╗╔╦╗╦
+    //  ╠═╣ ║ ║║║║
+    //  ╩ ╩ ╩ ╩ ╩╩═╝
+    template: `
+    <div class="team-member">
+        <p><strong>{{ name }}</strong></p>
+        <p><em>{{ role }}</em></p>
+    </div>
+    `,
+  
+    //  ╦  ╦╔═╗╔═╗╔═╗╦ ╦╔═╗╦  ╔═╗
+    //  ║  ║╠╣ ║╣ ║  ╚╦╝║  ║  ║╣
+    //  ╩═╝╩╚  ╚═╝╚═╝ ╩ ╚═╝╩═╝╚═╝
+    beforeMount: function() {
+      //…
+    },
+    mounted: async function(){
+      //…
+    },
+    beforeDestroy: function() {
+      //…
+    },
+  
+    //  ╦╔╗╔╔╦╗╔═╗╦═╗╔═╗╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ║║║║ ║ ║╣ ╠╦╝╠═╣║   ║ ║║ ║║║║╚═╗
+    //  ╩╝╚╝ ╩ ╚═╝╩╚═╩ ╩╚═╝ ╩ ╩╚═╝╝╚╝╚═╝
+    methods: {
+    }
+  });
+  
