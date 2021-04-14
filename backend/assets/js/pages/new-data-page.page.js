95122a4755b2f27973a465103baa4100bf5a82d1
erinRochfort99 <erochfor@uw.edu>
2021-04-14 08:38:05 -0700

    create data object for individual project page

--- backend/assets/js/pages/new-data-page.page.js
+++ backend/assets/js/pages/new-data-page.page.js
@@ -3,17 +3,57 @@ parasails.registerPage('new-data-page', {
   //  ║║║║║ ║ ║╠═╣║    ╚═╗ ║ ╠═╣ ║ ║╣
   //  ╩╝╚╝╩ ╩ ╩╩ ╩╩═╝  ╚═╝ ╩ ╩ ╩ ╩ ╚═╝
   data: {
-    //…
+    project: {
+      projectTitle: '',
+      image: null,
+      teamMembers: [
+        {
+          firstName: '',
+          lastName: '',
+          role: '',
+          optOut: false
+        },
+        {
+          firstName: '',
+          lastName: '',
+          role: '',
+          optOut: false
+        },
+        {
+          firstName: '',
+          lastName: '',
+          role: '',
+          optOut: false
+        },
+        {
+          firstName: '',
+          lastName: '',
+          role: '',
+          optOut: false
+        },
+      ],
+      challenge: '',
+      outcome: '',
+      impact: '',
+      problemStatement: '',
+      sponsored: false,
+      company: '',
+      majors: [],
+      introduction: '',
+      keyFeatures: '',
+      topics: []
+    },
   },
 
   //  ╦  ╦╔═╗╔═╗╔═╗╦ ╦╔═╗╦  ╔═╗
   //  ║  ║╠╣ ║╣ ║  ╚╦╝║  ║  ║╣
   //  ╩═╝╩╚  ╚═╝╚═╝ ╩ ╚═╝╩═╝╚═╝
   beforeMount: function() {
-    //…
+    // make api call to get all project info
   },
   mounted: async function() {
-    //…
+    console.log("mounted");
+    console.log(this.project);
   },
 
   //  ╦╔╗╔╔╦╗╔═╗╦═╗╔═╗╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
