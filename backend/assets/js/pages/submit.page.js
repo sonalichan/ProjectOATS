333699fbf4d253d2c0ea09568e7ecb9cde9dfd14
erinRochfort99 <erochfor@uw.edu>
2021-04-13 17:43:47 -0700

    created submit form

---
+++ backend/assets/js/pages/submit.page.js
@@ -0,0 +1,82 @@
+parasails.registerPage('submit', {
+  //  ╦╔╗╔╦╔╦╗╦╔═╗╦    ╔═╗╔╦╗╔═╗╔╦╗╔═╗
+  //  ║║║║║ ║ ║╠═╣║    ╚═╗ ║ ╠═╣ ║ ║╣
+  //  ╩╝╚╝╩ ╩ ╩╩ ╩╩═╝  ╚═╝ ╩ ╩ ╩ ╩ ╚═╝
+  data: {
+    form: {
+      projectTitle: '',
+      image: null,
+      teamMembers: [
+        {
+          firstName: '',
+          lastName: '',
+          role: '',
+          optOut: false
+        },
+        {
+          firstName: '',
+          lastName: '',
+          role: '',
+          optOut: false
+        },
+        {
+          firstName: '',
+          lastName: '',
+          role: '',
+          optOut: false
+        },
+        {
+          firstName: '',
+          lastName: '',
+          role: '',
+          optOut: false
+        },
+      ],
+      challenge: '',
+      outcome: '',
+      impact: '',
+      problemStatement: '',
+      sponsored: false,
+      company: '',
+      majors: [],
+      introduction: '',
+      keyFeatures: '',
+      topics: []
+    },
+    topics: [{ text: 'Select up to three', value: null }, 'Data Visualization', 'Ethics', 'UX Design', 'AI'],
+    majors: [{ text: 'Select major(s) of team members', value: null}, "INFO", "MLIS", "MSIM"],
+  },
+
+  //  ╦  ╦╔═╗╔═╗╔═╗╦ ╦╔═╗╦  ╔═╗
+  //  ║  ║╠╣ ║╣ ║  ╚╦╝║  ║  ║╣
+  //  ╩═╝╩╚  ╚═╝╚═╝ ╩ ╚═╝╩═╝╚═╝
+  beforeMount: function() {
+    //…
+  },
+  mounted: async function() {
+    //…
+  },
+
+  //  ╦╔╗╔╔╦╗╔═╗╦═╗╔═╗╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+  //  ║║║║ ║ ║╣ ╠╦╝╠═╣║   ║ ║║ ║║║║╚═╗
+  //  ╩╝╚╝ ╩ ╚═╝╩╚═╩ ╩╚═╝ ╩ ╩╚═╝╝╚╝╚═╝
+  methods: {
+    onSubmit(event) {
+      event.preventDefault()
+      alert(JSON.stringify(this.form))
+
+      // TODO: form validation
+      console.log(this.form);
+
+      // navigate to individual project page
+      this.goto('new-data-page', this.form);
+    },
+    processFile() {
+      // save file to variable here
+      console.log("process file");
+      console.log(this);
+      console.log(this.$get('$router'))
+      // this.goto('new-data-page', this.form)
+    }
+  }
+});
