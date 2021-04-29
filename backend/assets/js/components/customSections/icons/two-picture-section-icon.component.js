ca43055b5a39bd1da9567c87d32f9b4a2221bd15
erinRochfort99 <erochfor@uw.edu>
2021-04-29 16:31:49 -0700

    customized section icons

---
+++ backend/assets/js/components/customSections/icons/two-picture-section-icon.component.js
@@ -0,0 +1,73 @@
+/**
+ * TODO: change the component name to match--use kebab-case here
+ * This is how you will reference your component
+ * 
+ * <two-picture-section-icon>
+ * -----------------------------------------------------------------------------
+ * A button with a built-in loading spinner.
+ *
+ * @type {Component}
+ *
+ * @event click   [emitted when clicked]
+ * -----------------------------------------------------------------------------
+ */
+
+// TODO: change the component name to match file name--use camelCase here
+parasails.registerComponent('twoPictureSectionIcon', {
+    //  ╔═╗╦═╗╔═╗╔═╗╔═╗
+    //  ╠═╝╠╦╝║ ║╠═╝╚═╗
+    //  ╩  ╩╚═╚═╝╩  ╚═╝
+    props: [
+    ],
+  
+    //  ╦╔╗╔╦╔╦╗╦╔═╗╦    ╔═╗╔╦╗╔═╗╔╦╗╔═╗
+    //  ║║║║║ ║ ║╠═╣║    ╚═╗ ║ ╠═╣ ║ ║╣
+    //  ╩╝╚╝╩ ╩ ╩╩ ╩╩═╝  ╚═╝ ╩ ╩ ╩ ╩ ╚═╝
+    data: function (){
+      return {
+        //…
+      };
+    },
+  
+    //  ╦ ╦╔╦╗╔╦╗╦
+    //  ╠═╣ ║ ║║║║
+    //  ╩ ╩ ╩ ╩ ╩╩═╝
+    template: `
+    <div aria-hidden="true">
+        <div class="outer-box d-flex justify-content-between">
+          <div class="image-box">
+            <div class="icon-container">
+              <i class="fa fa-picture-o" aria-hidden="true"></i>
+              <div><small>Insert picture here...</small></div>
+            </div>
+          </div>
+          <div class="image-box">
+            <div class="icon-container">
+              <i class="fa fa-picture-o" aria-hidden="true"></i>
+              <div><small>Insert picture here...</small></div>
+            </div>
+          </div>
+        </div>
+    </div>
+    `,
+  
+    //  ╦  ╦╔═╗╔═╗╔═╗╦ ╦╔═╗╦  ╔═╗
+    //  ║  ║╠╣ ║╣ ║  ╚╦╝║  ║  ║╣
+    //  ╩═╝╩╚  ╚═╝╚═╝ ╩ ╚═╝╩═╝╚═╝
+    beforeMount: function() {
+      //…
+    },
+    mounted: async function(){
+      //…
+    },
+    beforeDestroy: function() {
+      //…
+    },
+  
+    //  ╦╔╗╔╔╦╗╔═╗╦═╗╔═╗╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ║║║║ ║ ║╣ ╠╦╝╠═╣║   ║ ║║ ║║║║╚═╗
+    //  ╩╝╚╝ ╩ ╚═╝╩╚═╩ ╩╚═╝ ╩ ╩╚═╝╝╚╝╚═╝
+    methods: {
+    }
+  });
+  
