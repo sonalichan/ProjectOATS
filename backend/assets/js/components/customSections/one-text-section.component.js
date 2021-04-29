32ab7229772c44cc2a552f184790916e8bccc2ae
erinRochfort99 <erochfor@uw.edu>
2021-04-29 16:32:19 -0700

    started making custom section components

---
+++ backend/assets/js/components/customSections/one-text-section.component.js
@@ -0,0 +1,61 @@
+/**
+ * TODO: change the component name to match--use kebab-case here
+ * This is how you will reference your component
+ * 
+ * <one-text-section>
+ * -----------------------------------------------------------------------------
+ * A button with a built-in loading spinner.
+ *
+ * @type {Component}
+ *
+ * @event click   [emitted when clicked]
+ * -----------------------------------------------------------------------------
+ */
+
+// TODO: change the component name to match file name--use camelCase here
+parasails.registerComponent('oneTextSection', {
+    //  ╔═╗╦═╗╔═╗╔═╗╔═╗
+    //  ╠═╝╠╦╝║ ║╠═╝╚═╗
+    //  ╩  ╩╚═╚═╝╩  ╚═╝
+    props: [
+      "text",
+    ],
+  
+    //  ╦╔╗╔╦╔╦╗╦╔═╗╦    ╔═╗╔╦╗╔═╗╔╦╗╔═╗
+    //  ║║║║║ ║ ║╠═╣║    ╚═╗ ║ ╠═╣ ║ ║╣
+    //  ╩╝╚╝╩ ╩ ╩╩ ╩╩═╝  ╚═╝ ╩ ╩ ╩ ╩ ╚═╝
+    data: function (){
+      return {
+        //…
+      };
+    },
+  
+    //  ╦ ╦╔╦╗╔╦╗╦
+    //  ╠═╣ ║ ║║║║
+    //  ╩ ╩ ╩ ╩ ╩╩═╝
+    template: `
+    <div>
+        {{ text }}
+    </div>
+    `,
+  
+    //  ╦  ╦╔═╗╔═╗╔═╗╦ ╦╔═╗╦  ╔═╗
+    //  ║  ║╠╣ ║╣ ║  ╚╦╝║  ║  ║╣
+    //  ╩═╝╩╚  ╚═╝╚═╝ ╩ ╚═╝╩═╝╚═╝
+    beforeMount: function() {
+      //…
+    },
+    mounted: async function(){
+      //…
+    },
+    beforeDestroy: function() {
+      //…
+    },
+  
+    //  ╦╔╗╔╔╦╗╔═╗╦═╗╔═╗╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ║║║║ ║ ║╣ ╠╦╝╠═╣║   ║ ║║ ║║║║╚═╗
+    //  ╩╝╚╝ ╩ ╚═╝╩╚═╩ ╩╚═╝ ╩ ╩╚═╝╝╚╝╚═╝
+    methods: {
+    }
+  });
+  
---
+++ backend/assets/js/components/customSections/two-text-section.component.js
@@ -0,0 +1,67 @@
+/**
+ * TODO: change the component name to match--use kebab-case here
+ * This is how you will reference your component
+ * 
+ * <two-text-section>
+ * -----------------------------------------------------------------------------
+ * A button with a built-in loading spinner.
+ *
+ * @type {Component}
+ *
+ * @event click   [emitted when clicked]
+ * -----------------------------------------------------------------------------
+ */
+
+// TODO: change the component name to match file name--use camelCase here
+parasails.registerComponent('twoTextSection', {
+    //  ╔═╗╦═╗╔═╗╔═╗╔═╗
+    //  ╠═╝╠╦╝║ ║╠═╝╚═╗
+    //  ╩  ╩╚═╚═╝╩  ╚═╝
+    props: [
+      "text1",
+      "text2"
+    ],
+  
+    //  ╦╔╗╔╦╔╦╗╦╔═╗╦    ╔═╗╔╦╗╔═╗╔╦╗╔═╗
+    //  ║║║║║ ║ ║╠═╣║    ╚═╗ ║ ╠═╣ ║ ║╣
+    //  ╩╝╚╝╩ ╩ ╩╩ ╩╩═╝  ╚═╝ ╩ ╩ ╩ ╩ ╚═╝
+    data: function (){
+      return {
+        //…
+      };
+    },
+  
+    //  ╦ ╦╔╦╗╔╦╗╦
+    //  ╠═╣ ║ ║║║║
+    //  ╩ ╩ ╩ ╩ ╩╩═╝
+    template: `
+    <div class="d-flex justify-content-between">
+        <div>
+            {{ text1 }}
+        </div>
+        <div>
+            {{ text2}}
+        </div>
+    </div>
+    `,
+  
+    //  ╦  ╦╔═╗╔═╗╔═╗╦ ╦╔═╗╦  ╔═╗
+    //  ║  ║╠╣ ║╣ ║  ╚╦╝║  ║  ║╣
+    //  ╩═╝╩╚  ╚═╝╚═╝ ╩ ╚═╝╩═╝╚═╝
+    beforeMount: function() {
+      //…
+    },
+    mounted: async function(){
+      //…
+    },
+    beforeDestroy: function() {
+      //…
+    },
+  
+    //  ╦╔╗╔╔╦╗╔═╗╦═╗╔═╗╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ║║║║ ║ ║╣ ╠╦╝╠═╣║   ║ ║║ ║║║║╚═╗
+    //  ╩╝╚╝ ╩ ╚═╝╩╚═╩ ╩╚═╝ ╩ ╩╚═╝╝╚╝╚═╝
+    methods: {
+    }
+  });
+  
