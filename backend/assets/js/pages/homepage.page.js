4a6a90ce31a5cf7408578719d44351ffe8716f91
Michael Doyle <mikaldoyle@gmail.com>
2021-05-05 12:54:54 -0700

    Data Update

--- backend/assets/js/pages/homepage.page.js
+++ backend/assets/js/pages/homepage.page.js
@@ -14,12 +14,14 @@ parasails.registerPage('homepage', {
     axios.get('/api/v1/project'
       ).then((resp) => {
           this.projects = resp.data;
+          console.log((this.projects[0].createdAt).asDate())
        }).catch((err) => {
         console.log(err);
     })
+    
   },
   mounted: async function(){
-    this._setHeroHeight();
+    
   },
 
   //  ╦╔╗╔╔╦╗╔═╗╦═╗╔═╗╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
@@ -27,22 +29,7 @@ parasails.registerPage('homepage', {
   //  ╩╝╚╝ ╩ ╚═╝╩╚═╩ ╩╚═╝ ╩ ╩╚═╝╝╚╝╚═╝
   methods: {
 
-    clickHeroButton: async function() {
-      // Scroll to the 'get started' section:
-      $('html, body').animate({
-        scrollTop: this.$find('[purpose="scroll-destination"]').offset().top
-      }, 500);
-    },
-
-    // Private methods not tied to a particular DOM event are prefixed with _
-    _setHeroHeight: function() {
-      var $hero = this.$find('[purpose="full-page-hero"]');
-      var headerHeight = $('[purpose="page-header"]').outerHeight();
-      var heightToSet = $(window).height();
-      heightToSet = Math.max(heightToSet, 500);//« ensure min height of 500px - header height
-      heightToSet = Math.min(heightToSet, 1000);//« ensure max height of 1000px - header height
-      $hero.css('min-height', heightToSet - headerHeight+'px');
-    },
+    
 
   }
 });
