ada1b2d51a5e86505b16b71a6079dcbd2dc7cbc9
erinRochfort99 <erochfor@uw.edu>
2021-05-19 14:02:45 -0700

    clean up routes api endpoints

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -34,17 +34,6 @@ module.exports.routes = {
   //  ╔═╗╔═╗╦  ╔═╗╔╗╔╔╦╗╔═╗╔═╗╦╔╗╔╔╦╗╔═╗
   //  ╠═╣╠═╝║  ║╣ ║║║ ║║╠═╝║ ║║║║║ ║ ╚═╗
   //  ╩ ╩╩  ╩  ╚═╝╝╚╝═╩╝╩  ╚═╝╩╝╚╝ ╩ ╚═╝
-
-  // '/api/v1/account/logout':                           { action: 'account/logout' },
-  // 'PUT   /api/v1/account/update-password':            { action: 'account/update-password' },
-  // 'PUT   /api/v1/account/update-profile':             { action: 'account/update-profile' },
-  // 'PUT   /api/v1/account/update-billing-card':        { action: 'account/update-billing-card' },
-  // 'PUT   /api/v1/entrance/login':                        { action: 'entrance/login' },
-  // 'POST  /api/v1/entrance/signup':                       { action: 'entrance/signup' },
-  // 'POST  /api/v1/entrance/send-password-recovery-email': { action: 'entrance/send-password-recovery-email' },
-  // 'POST  /api/v1/entrance/update-password-and-login':    { action: 'entrance/update-password-and-login' },
-  // 'POST  /api/v1/deliver-contact-form-message':          { action: 'deliver-contact-form-message' },
-  // 'POST  /api/v1/project':                 { controller: 'ProjectController', action: 'createProject' },
   'POST  /api/v1/project':                 { action: 'project/create-project' },
   'GET /api/v1/project':                  { action: 'project/get-all-projects'},
   // 'GET /api/v1/project/:projectTitle':                  { action: 'project/get-project'},
